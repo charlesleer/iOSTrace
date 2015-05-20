d7144f6e9a37897ba5ec8702da5e2e22a42bab6c
Charles Leerink <Charles.Leerink@outlook.com>
2015-05-20 11:39:36 +0200

    test new method
    testing if we can do without the weird cut

--- iOSTrace.sh
+++ iOSTrace.sh
@@ -8,15 +8,15 @@
 #
 #find the UDID from systemprofiler assumming just 1 device is connected
 
-if [ $# -eq 0 ]
-  then
-    echo "Please start as sh iOSTrace.sh <number>"
-    echo "Where number is: 34 when using a USB hub that goes to the Mac or 29 when cable is directly connected to the Mac"
-    echo "eg: sh iOSTrace.sh 34"
-    exit 1
-fi
+#if [ $# -eq 0 ]
+#  then
+#    echo "Please start as sh iOSTrace.sh <number>"
+#    echo "Where number is: 34 when using a USB hub that goes to the Mac or 29 when cable is directly connected to the Mac"
+#    echo "eg: sh iOSTrace.sh 34"
+#    exit 1
+#fi
 
-UDID=`system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' | grep "Serial Number: " | cut -c $1-`
+UDID=`system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' | grep "Serial Number: " | | sed 's/.*Number://'
 clear
 echo "Easy iOS Trace v3"
 echo ""
